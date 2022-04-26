f8fcb2018383fdcf95bed670344e9cc681961e33
Oscar Salgado <oscarg.cdac@gmail.com>
2022-04-25 23:25:21 -0500

    cambios en el archivo de velocidad.py

--- velocidad.py
+++ velocidad.py
@@ -5,24 +5,16 @@ entregue la velocidad en kilómetros por hora y en metros por segundo.
 Para esto, existen dos variables tiempo y distancia que vienen en segundos y kilómetros respectivamente. 
 Tu programa debe guardar en la variable resultado un string
 '''
-
-
-
 def velocidad(distancia, tiempo):
-    resultado = ''
-    
-    velocidad = distancia/tiempo 
+    resultado = ""
+    velocidad = distancia/tiempo
 
-    a = round((float(velocidad) * 3600), 6)
-    b = round((float(velocidad)* 1000), 6)
-    
-
-    resultado = str(a) + " " "km/h" " " " o " " " + str(b) + " " " m/s "
-    print(f"La velocidad es {resultado}")
+    vel_kmh = velocidad * 3600
+    vel_ms = velocidad * 1000
+    resultado = f'la velocidad es {vel_kmh} km/h o {vel_ms} m/s'
+    print(f'La velocidad es {resultado}')
     return resultado
 tiempo = 1
 distancia = 0.01
 
 velocidad(distancia, tiempo)
-
-   
